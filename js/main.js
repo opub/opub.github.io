9b37aa5f8a1e5807d228d8ffaa04825a20835d17
Ted O'Connor <ted@opub.com>
2019-06-07 16:08:47 -0400

    added password utilities

--- js/main.js
+++ js/main.js
@@ -15,6 +15,54 @@ $(document).ready(function() {
   }); // initialize animate on scroll library
 });
 
+//password utilities
+const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWER = 'abcdefghijklmnopqrstuvwxyz';
+const NUMBER = '0123456789';
+const SYMBOL = '~`!@#$%^&*()-=_+{}|[]\\:";\'<>?,./';
+function generatePasswords() {
+    let chars = '';
+    if ($('#pwupper').is(':checked')) chars += UPPER;
+    if ($('#pwlower').is(':checked')) chars += LOWER;
+    if ($('#pwnumber').is(':checked')) chars += NUMBER;
+    if ($('#pwsymbol').is(':checked')) chars += SYMBOL;
+    if ($('#pwreadable').is(':checked')) chars = chars.replace(/[0O1lI\|]/g, '');
+    let size = $('#pwlength').val();
+    let pwds = '';
+    if (chars.length > 0) {
+        for (var i=0; i < 5; i++) {
+            for (var j=0; j < size; j++ ) {
+                pwds += chars[Math.floor(Math.random() * chars.length)];
+            }
+            if (i < 4) pwds += '\n\n';
+        }
+    }
+    else {
+        pwds = 'Select at least one type of character to include.'
+    }
+    $('#passwordlist').text(pwds);
+}
+function checkPassword() {
+    let pwd = $('#pwstrength').val();
+    let result = zxcvbn(pwd);
+    $('#passwordrating').attr('class', 'strength' + result.score);
+    let rating = 'DO NOT USE THIS';
+    switch (result.score) {
+        case 1: rating = 'Pretty Weak'; break;
+        case 2: rating = 'You Can Do Better'; break;
+        case 3: rating = 'Better'; break;
+        case 4: rating = 'Very Good'; break;
+    }
+    $('#passwordrating').text(rating);
+    let feedback = '';
+    if (result.score <= 2) {
+        feedback += result.feedback.warning;
+        if (feedback.length > 0) feedback += '<br>';
+        feedback += result.feedback.suggestions.join('<br>');
+    }
+    $('#passwordfeedback').html(feedback);
+}
+
 // Smooth scroll for links with hashes
 $('a.smooth-scroll')
 .click(function(event) {
